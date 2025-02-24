137e3945cc7bd70535b80722ffee264ef26e6eff
Nicolas Arrieta <n.arrietac@uniandes.edu.co>
2025-02-24 12:34:09 -0500

    preparación del dash para MV

--- app/appv2.py
+++ app/appv2.py
@@ -437,5 +437,5 @@ def update_output(n_clicks, sf, rooms, baths, city, amenities, pets):
     return estimate_value, diff_est, mean_makt, min_makt, max_makt, grap, m
 
 if __name__ == '__main__':
-    # app.run_server(host = "0.0.0.0", debug=True)
-    app.run_server(debug=True)
+    app.run_server(host = "0.0.0.0", debug=True)
+    # app.run_server(debug=True)
